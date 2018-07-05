0f1210699ac2c80a30bac78aed628c4c612f37fa
Viswa <viswanath3344@gmail.com>
2018-07-05 15:05:06 +0530

    Initial Commit

---
+++ VedasRandomGenerator/VedasRandomGenerator.h
@@ -0,0 +1,19 @@
+//
+//  VedasRandomGenerator.h
+//  VedasRandomGenerator
+//
+//  Created by Ming-En Liu on 05/07/18.
+//  Copyright © 2018 Vedas labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VedasRandomGenerator.
+FOUNDATION_EXPORT double VedasRandomGeneratorVersionNumber;
+
+//! Project version string for VedasRandomGenerator.
+FOUNDATION_EXPORT const unsigned char VedasRandomGeneratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VedasRandomGenerator/PublicHeader.h>
+
+
